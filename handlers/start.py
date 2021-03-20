3238f1deca3414fd1e3ae59e711a3574d6606633
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-03-20 12:41:38 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -8,22 +8,22 @@ from helpers.filters import command, other_filters, other_filters2
 async def start(_, message: Message):
     await message.reply_text(
         f"""🙃 Hi {message.from_user.first_name}!
-✨ I am Hêllẞø† Music Player. 
+✨ I am Dèvílẞø† Music Player. 
 🥳 I can play music in your Telegram Group's Voice Chat😉
 ⚜️ Use these buttons below to know more. 👇""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "⚒ Source code", url="https://github.com/TheVaders/MusicBot"
+                        "⚒ Source code", url="t.me/lucifermorningstarbackup"
                     )
                 ],
                 [
                     InlineKeyboardButton(
-                        "💬 Group", url="https://t.me/hellbot_official_chat"
+                        "💬 Group", url="https://t.me/deviluserbot"
                     ),
                     InlineKeyboardButton(
-                        "Channel 🔈", url="https://t.me/hellbot_official"
+                        "Channel 🔈", url="https://t.me/deviluserbot"
                     )
                 ],
                 [
@@ -39,7 +39,7 @@ async def start(_, message: Message):
 @Client.on_message(command("start") & other_filters)
 async def start2(_, message: Message):
     await message.reply_text(
-        "**Hêllẞø†:** I'm Working!!!\nUse me in Inline to search for a YouTube Video/Music. \n**Happy Streaming**",
+        "**DÈVÍlẞø†:** I'm Working!!!\nUse me in Inline to search for a YouTube Video/Music. \n**Happy Streaming**",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
